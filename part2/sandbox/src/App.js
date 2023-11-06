1c4248310ab433a130a3c65f21f4697be9ee1f60
Justin822 <93686541+Justin822@users.noreply.github.com>
2023-11-06 21:35:05 +0100

    Hello Notes

--- part2/sandbox/src/App.js
+++ part2/sandbox/src/App.js
@@ -3,7 +3,7 @@ import Note from "./components/Note";
 const App = ({ notes }) => {
   return (
     <div>
-      <h1>Notes</h1>
+      <h1>Hello Notes</h1>
       <ul>
         {notes.map((note) => (
           <Note key={note.id} note={note} />
