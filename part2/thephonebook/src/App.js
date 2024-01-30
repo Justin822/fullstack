f4a0548d41266cdc4eb3e4e70c30241cde83757d
Justin822 <93686541+Justin822@users.noreply.github.com>
2024-01-30 23:20:53 +0100

    exercises 2.12 - 2.15

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -1,17 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ItemList from "./components/ItemList";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
 
+import contactService from "./services/contacts";
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phone: "0612345678", id: 1 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
 
   const [searchItem, setSearchItem] = useState("");
   const [filteredUsers, setFilteredUsers] = useState(persons);
+
+  useEffect(() => {
+    contactService.getAll().then((res) => {
+      setPersons(res.data);
+    });
+  }, []);
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
@@ -37,16 +43,36 @@ const App = () => {
     const personObject = {
       name: newName,
       phone: newNumber,
-      id: persons.length + 1,
     };
 
     const nameExists = persons.some(function (ele) {
       return ele.name === personObject.name;
     });
 
+    const replaceNumber = () => {
+      if (
+        window.confirm(`${personObject.name} is already added to the phonebook`)
+      ) {
+        persons.some(function (ele) {
+          const isSame = ele.name === personObject.name;
+          return isSame
+            ? contactService.update(ele.id, personObject)
+            : console.log("no");
+        });
+      }
+    };
+
     nameExists
-      ? alert(`${personObject.name} is already added to the phonebook`)
-      : setPersons(persons.concat(personObject));
+      ? replaceNumber()
+      : contactService.create(personObject).then((res) => {
+          setPersons(persons.concat(res.data));
+        });
+
+    // axios.post("http://localhost:3001/contacts", personObject).then((res) => {
+    //   setPersons(persons.concat(res.data));
+    // });
+
+    // setPersons(persons.concat(personObject))
 
     setNewName("");
     setNewNumber("");
--- part2/thephonebook/src/components/ItemList.jsx
+++ part2/thephonebook/src/components/ItemList.jsx
@@ -1,10 +1,22 @@
+import axios from "axios";
+import contactService from "../services/contacts";
+
 const ItemList = ({ items }) => {
+  const deleteContact = (name, id) => {
+    if (window.confirm(`Delete ${name}?`)) {
+      contactService.deleteContactService(id);
+    }
+  };
+
   return (
     <>
       {items.map((item) => {
         return (
           <div key={item.id}>
-            {item.name} {item.phone}
+            {item.name} {item.phone}{" "}
+            <button onClick={() => deleteContact(item.name, item.id)}>
+              Delete
+            </button>
           </div>
         );
       })}
