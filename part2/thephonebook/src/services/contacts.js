84a7ddf4834e124208393ecf1faac0f84d80c718
Justin Kuijpers <justin@justinkuijpers.com>
2024-03-21 21:04:28 +0100

    part3

--- part2/thephonebook/src/services/contacts.js
+++ part2/thephonebook/src/services/contacts.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/contacts";
+const baseUrl = "/api/contacts";
 
 const getAll = () => {
-  return axios.get(baseUrl);
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
 };
 
 const create = (newObject) => {
